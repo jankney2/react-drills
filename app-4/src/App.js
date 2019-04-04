4b422ef1e780b927b21090aa6e592bb5f74c0c40
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-04-04 08:38:40 -0600

    app4 finish

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state= {
+      username: '', 
+      password:"", 
+    }
+  }
+  
+clickHandler= () => {
+  alert(`${this.state.username} logged in with ${this.state.password}`)
+}
+
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+      <input type= "email" placeholder= "username" onChange= {(e) => {
+        this.setState({
+          username: e.target.value
+        })
+      }}/>
+      <input type= "password" placeholder= "password" onChange= {(e) => {
+        this.setState({
+         password: e.target.value
+        })
+      }}/>
+      <button onClick={()=>{this.clickHandler()}}>Login!</button>
+
+
       </div>
     );
   }
